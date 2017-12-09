c10d5fd70af441389cb374dc62f661e8fc6be555
Rui Campos <ruir2c@gmail.com>
2017-12-09 14:48:16 +0100

    added watch directory method

---
+++ test/utils/test_watchdirectory.js
@@ -0,0 +1,85 @@
+"use strict";
+/* eslint-env mocha */
+
+const tmp = require("tmp");
+const fse = require("fs-extra");
+
+const watchdirectory = require("../../lib/utils/watchdirectory");
+
+describe("watchdirectory", function () {
+
+    let tmpDir = null;
+    beforeEach(function () {
+        tmpDir = tmp.dirSync({"unsafeCleanup": true});
+        fse.copySync(__dirname + "/../../test_directories/test_watchdirectory", tmpDir.name);
+    });
+
+    afterEach(function (done) {
+        fse.remove(tmpDir.name, done);
+    });
+
+    function testOneDirChange(changeMethod, done) {
+    
+        let doneCalled = false;
+        const cancel = watchdirectory.watchForAChange(tmpDir.name, function () {
+            doneCalled = true;
+            done();
+        });
+        
+        setTimeout(function () {
+            changeMethod();
+    
+            setTimeout(function() {
+                cancel.cancel();
+                if (!doneCalled)
+                {
+                    done(new Error("change not triggered"));
+                }
+            }, 500);
+        }, 500);
+    }
+
+    function touchFile(file) {
+
+        const logStream = fse.createWriteStream(file, {"flags": "a"});
+        logStream.write(" new content");
+        logStream.end("");
+    }
+
+    it("file change should trigger", function (done) {
+        testOneDirChange(function () {
+            touchFile(tmpDir.name + "/file1.txt");
+        }, done);
+    });
+
+    it("file change inside dir should trigger", function (done) {
+        testOneDirChange(function () {
+            touchFile(tmpDir.name + "/dir/file2.txt");
+        }, done);
+    });
+
+
+    it("add file change should trigger", function (done) {
+        testOneDirChange(function () {
+            fse.writeFileSync(tmpDir.name + "/newfile.txt");
+        }, done);
+    });
+
+
+    it("add dir change should trigger", function (done) {
+        testOneDirChange(function () {
+            fse.mkdirSync(tmpDir.name + "/newdir/");
+        }, done);
+    });
+
+    it("no change should not trigger", function (done) {
+        const cancel = watchdirectory.watchForAChange(tmpDir.name, function () {
+            done(new Error("shouldn't have changed"));
+        });
+
+        setTimeout(function () {
+            cancel.cancel();
+            done();
+        }, 200);
+    });
+});

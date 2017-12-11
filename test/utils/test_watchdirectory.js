634ce64592599fff99fb5999cdeaf687043c1d32
Rui Campos <ruir2c@gmail.com>
2017-12-11 20:40:23 +0100

    watch method for one change was very difficult to make work, so now the watch method is something you enable once and handle the changes from it.

--- test/utils/test_watchdirectory.js
+++ test/utils/test_watchdirectory.js
@@ -5,50 +5,83 @@ const expect = require("chai").expect;
 const tmp = require("tmp");
 const fse = require("fs-extra");
 const VError = require("verror").VError;
+const pathutils = require("path");
 
 const watchdirectory = require("../../lib/utils/watchdirectory");
 
 describe("watchdirectory", function () {
 
+    this.timeout(5000);
+
     let tmpDir = null;
-    beforeEach(function () {
-        tmpDir = tmp.dirSync({"unsafeCleanup": true});
+    let currentCallback = null;
+    let watcher = null;
+    before(function () {
+        tmpDir = tmp.dirSync({"keep": true});
+        watcher = watchdirectory.watchForChanges(tmpDir.name, function (ev, path, stat) {
+            if (currentCallback != null) {
+                currentCallback(ev, path, stat);
+            }
+        });
+    });
+    beforeEach(function (done) {
         fse.copySync(__dirname + "/../../test_directories/test_watchdirectory", tmpDir.name);
+        setTimeout(done, 3000); // make sure all changes are flushed
     });
 
     afterEach(function (done) {
-        fse.remove(tmpDir.name, done);
+
+        const files = fse.readdirSync(tmpDir.name);
+        for (const i in files) {
+            fse.removeSync(pathutils.join(tmpDir.name, files[i]));
+        }
+
+        setTimeout(done, 3000); // make sure all changes are flushed
+    });
+
+    after(function () {
+        watcher.cancel();
     });
 
     /**
-     * Watches the tmp directory, triggers the change method, and checks if the watch triggers the change
+     * Triggers the change method, and checks if the watch triggers the change
      * @param {function} changeMethod - The method that changes
      * @param {function} done - Called after it finishes testing
+     * @return {undefined}
      */
-    function testOneDirChange(changeMethod, done) {
-    
+    function testOneDirChange(changeMethod, expectedEvent, expectedPath, done) {
         let doneCalled = false;
-        const cancel = watchdirectory.watchForAChange(tmpDir.name, function () {
+        currentCallback = function (ev, path, stat) {
+            if (ev != expectedEvent) return;
+
+            expect(path).to.be.equal(expectedPath);
+            if (ev != "unlink" && ev != "unlinkDir") {
+                expect(stat).to.not.be.undefined;
+            }
+            else {
+                expect(stat).to.be.undefined;
+            }
             doneCalled = true;
+            currentCallback = null;
             done();
-        });
+        };
         
-        setTimeout(function () {
-            changeMethod();
-    
-            setTimeout(function() {
-                cancel.cancel();
-                if (!doneCalled)
-                {
-                    done(new Error("change not triggered"));
-                }
-            }, 500);
-        }, 500);
+        changeMethod();
+        
+        setTimeout(function() {
+            if (!doneCalled)
+            {
+                currentCallback = null;
+                doneCalled = true;
+                done(new Error("change not triggered"));
+            }
+        }, 3000);
     }
 
     /**
      * Appends "new content" to a file.
      * @param {string} file - The directory to watch
+     * @return {undefined}
      */
     function touchFile(file) {
 
@@ -59,45 +92,61 @@ describe("watchdirectory", function () {
 
     it("test parameters", function () {
 
-        expect(watchdirectory.watchForAChange.bind(null, null, null)).to.throw(VError);
-        expect(watchdirectory.watchForAChange.bind(null, tmpDir.name, null)).to.throw(VError);
-        expect(watchdirectory.watchForAChange.bind(null, "dir that doesn't exist", function() {})).to.throw(VError);
+        expect(watchdirectory.watchForChanges.bind(null, null, null)).to.throw(VError);
+        expect(watchdirectory.watchForChanges.bind(null, tmpDir.name, null)).to.throw(VError);
+        expect(watchdirectory.watchForChanges.bind(null, "dir that doesn't exist", function() {})).to.throw(VError);
     });
 
     it("file change should trigger", function (done) {
+        const path = pathutils.join(tmpDir.name, "file1.txt");
         testOneDirChange(function () {
-            touchFile(tmpDir.name + "/file1.txt");
-        }, done);
+            touchFile(path);
+        }, "change", path, done);
     });
 
     it("file change inside dir should trigger", function (done) {
+        const path = pathutils.join(tmpDir.name, "dir", "file2.txt");
         testOneDirChange(function () {
-            touchFile(tmpDir.name + "/dir/file2.txt");
-        }, done);
+            touchFile(path);
+        }, "change", path, done);
     });
 
 
     it("add file change should trigger", function (done) {
+        const path = pathutils.join(tmpDir.name, "newfile.txt");
         testOneDirChange(function () {
-            fse.writeFileSync(tmpDir.name + "/newfile.txt");
-        }, done);
+            fse.writeFileSync(path);
+        }, "add", path, done);
     });
 
-
     it("add dir change should trigger", function (done) {
+        const path = pathutils.join(tmpDir.name, "newdir");
         testOneDirChange(function () {
-            fse.mkdirSync(tmpDir.name + "/newdir/");
-        }, done);
+            fse.mkdirSync(path);
+        }, "addDir", path, done);
+    });
+    it("remove dir change should trigger", function (done) {
+        const path = pathutils.join(tmpDir.name, "dir");
+        testOneDirChange(function () {
+            fse.removeSync(path);
+        }, "unlinkDir", path, done);
+    });
+
+    it("remove file change should trigger", function (done) {
+        const path = pathutils.join(tmpDir.name, "file1.txt");
+        testOneDirChange(function () {
+            fse.removeSync(path);
+        }, "unlink", path, done);
     });
 
     it("no change should not trigger", function (done) {
-        const cancel = watchdirectory.watchForAChange(tmpDir.name, function () {
+        currentCallback = function () {
             done(new Error("shouldn't have changed"));
-        });
+        };
 
         setTimeout(function () {
-            cancel.cancel();
+            currentCallback = null;
             done();
-        }, 200);
+        }, 4000);
     });
 });

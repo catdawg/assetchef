0ecb8a228e2a2ba84c465b248d8440d3646267de
Rui Campos <ruir2c@gmail.com>
2018-11-12 19:10:03 +0100

    added pathinterfacecombination

--- src/plugin/ipathtreereadonly.ts
+++ src/plugin/ipathtreereadonly.ts
@@ -1,8 +1,8 @@
 import { IPathChangeEvent } from "./ipathchangeevent";
 
 /**
- * Interface used to pass around an API that looks into a tree structure like a filesystem.
- * This can be used to abstract an actual filesystem or something that resembles it.
+ * Interface used to pass around an API that looks into a tree structure accessed with a path
+ * resembling a filesystem.
  */
 export interface IPathTreeReadonly<TContent>  {
     /**
@@ -13,7 +13,7 @@ export interface IPathTreeReadonly<TContent>  {
     listenChanges(cb: (ev: IPathChangeEvent) => void): {unlisten: () => void};
 
     /**
-     * Iterator for the paths inside a specified path. If the path doesn't exist, the behaviour is unspecified.
+     * Iterator for the paths inside a specified path. If the path doesn't exist, it should throw an exception
      * @param path the 'folder' where the files are
      */
     list(path: string): IterableIterator<string>;
@@ -24,7 +24,7 @@ export interface IPathTreeReadonly<TContent>  {
     listAll(): IterableIterator<string>;
 
     /**
-     * Check if a path is a directory. If the path doesn't exists the behaviour is unspecified.
+     * Check if a path is a directory. If the path doesn't exist, it should throw an exception
      * @param path the path
      */
     isDir(path: string): boolean;
@@ -36,7 +36,7 @@ export interface IPathTreeReadonly<TContent>  {
     exists(path: string): boolean;
 
     /**
-     * Get the content of a path, if it doesn't exist the behaviour is unspecified.
+     * Get the content of a path, if it doesn't exist, it should throw an exception
      * @param path the path
      */
     get(path: string): TContent;
